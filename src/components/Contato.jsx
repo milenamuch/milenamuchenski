65c37c8fc72eb796a54b87ed9334af31bb273cde
Milena Madeira <milenamuchenski@gmail.com>
2022-11-03 21:56:17 -0300

    commit3

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,45 +1,31 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
-export default function Contato() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+export default function HelperTextAligned() {
+  <h1 className="texto-centralizado">Sobre</h1>;
   return (
-    <div>
-      <Modal
-        keepMounted
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="keep-mounted-modal-title"
-        aria-describedby="keep-mounted-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
-            Deixe seu contato
-          </Typography>
-          <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+    <Box
+      sx={{
+        marginTop: 30,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>Contato</h1>
+      <TextField helperText=" " id="demo-helper-text-aligned" label="Nome" />
+      <TextField
+        helperText=" "
+        id="demo-helper-text-aligned-no-helper"
+        label="Email"
+      />
+
+      <TextField
+        helperText=" "
+        id="demo-helper-text-aligned-no-helper"
+        label="Telefone"
+      />
+    </Box>
   );
 }
-
--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,85 +1,87 @@
 import { ContactMail, ContactPage } from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { AppBar, Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { Link, Outlet, useRouteError } from "react-router-dom";
 
 export const PageLayout = () => {
+  const DrawerApp = () => {
+    return (
+      <Box style={{ minWitch: "10px " }}>
+        <Typography variant="h5"></Typography>
+        <List>
+          <ListItem>
+            <ListItemButton component="a" href="/">
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="/sobre">
+              <ListItemText primary="Sobre" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="/albuns">
+              <ListItemText primary="Álbuns" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="/noticias">
+              <ListItemText primary="Notícias" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="/contato">
+              <ListItemText primary="Contato" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    );
+  };
 
-    const DrawerApp = () => {
-        return (
-            <Box style={{ minWitch: "10px " }}>
-                <Typography variant="h5">
-                </Typography>
-                <List>
-                    <ListItem>
-                        <ListItemButton component="a" href="/">
-                            <ListItemText primary="Home" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton component="a" href="/sobre">
-                            <ListItemText primary="Sobre" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton component="a" href="/albuns">
-                            <ListItemText primary="Álbuns" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton component="a" href="/noticias">
-                            <ListItemText primary="Notícias" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton component="a" href="/contato">
-                            <ListItemText primary="Contato" />
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-            </Box>
-        )
-    }
+  const ContainerMenu = () => {
+    const [aberto, setAberto] = useState(false);
 
-    const ContainerMenu = () => {
-        const [aberto, setAberto] = useState(false)
-
-        const lideComOMenu = () => {
-            setAberto(!aberto)
-        }
-
-        return (
-            <Box>
-                <AppBar>
-                    <Toolbar>
-                        <IconButton
-                            onClick={lideComOMenu}
-                        >
-                            <MenuIcon></MenuIcon>
-                            
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-                <Box>
-                    <Drawer
-                        open={aberto}
-                        onClose={lideComOMenu}
-                    >
-                        <DrawerApp />
-                    </Drawer>
-                </Box>
-            </Box>
-        );
+    const lideComOMenu = () => {
+      setAberto(!aberto);
     };
 
     return (
-        <>
-            <ContainerMenu />
-            <Box style={{ marginTop: "80px" }}>
-                <Outlet />
-            </Box>
-
-        </>
+      <Box>
+        <AppBar>
+          <Toolbar>
+            <IconButton onClick={lideComOMenu}>
+              <MenuIcon></MenuIcon>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Box>
+          <Drawer open={aberto} onClose={lideComOMenu}>
+            <DrawerApp />
+          </Drawer>
+        </Box>
+      </Box>
     );
+  };
+
+  return (
+    <>
+      <ContainerMenu />
+      <Box style={{ marginTop: "80px" }}>
+        <Outlet />
+      </Box>
+    </>
+  );
 };
